7593146aadecc347ec6e10e2d522af16caedb38a
Geancarlos Chamorro Espinoza <geancarlosce96@gmail.com>
2017-12-28 01:11:18 -0500

    Migracion Categoria y Movimiento Relacionado

---
+++ 2017_12_28_054912_create_movements_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMovementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type',50);
+            $table->dateTime('movement_date');
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('user_id');
+            $table->string('description',1000);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movements');
+    }
+}
